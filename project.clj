8c744961a9fccd31e67782a738f7f9d98f4f425f
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-09-07 14:41:05 -0500

    0.2.0 - api change to confirm to clj-http standards

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-couchdb "0.1.0"
+(defproject clj-couchdb "0.2.0"
   :description "A very simple wrapper to access couchdb data."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [clj-http "0.1.3"]
