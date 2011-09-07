8de631a718f68c85387c7d32be412fe7cb62ffc4
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-09-07 12:01:33 -0500

    freezing at 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-couchdb "0.1.0-SNAPSHOT"
+(defproject clj-couchdb "0.1.0"
   :description "A very simple wrapper to access couchdb data."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [clj-http "0.1.3"]
