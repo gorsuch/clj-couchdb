155ec078eed3b91c6c01338ddca797b767e266b1
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-09-07 12:09:03 -0500

    we should try and be as clj-http friendly as possible

--- src/clj_couchdb/core.clj
+++ src/clj_couchdb/core.clj
@@ -3,13 +3,12 @@
 		      [clj-json.core :as json]))
 
 (defn fetch [base db id & [params]]
-	(json/parse-string (:body (client/get (str base "/" db "/" id) {:basic-auth [(:auth-user params) (:auth-pass params)]}))))
+	(json/parse-string (:body (client/get (str base "/" db "/" id) params))))
 
 (defn insert [base db doc & [params]]
-	(client/post (str base "/" db) {:body (json/generate-string doc)
-		                            :basic-auth [(:auth-user params) (:auth-pass params)]
-		                            :content-type :json
-		                            :accept :json}))
+	(client/post (str base "/" db) (merge {:body (json/generate-string doc)
+		                                   :content-type :json
+		                                   :accept :json} params)))
 
 (defn view [base db design-doc name & [params]]
-	(json/parse-string (:body (client/get (str base "/" db "/_design/" design-doc "/_view/" name) {:basic-auth [(:auth-user params) (:auth-pass params)]}))))
+	(json/parse-string (:body (client/get (str base "/" db "/_design/" design-doc "/_view/" name) params))))
