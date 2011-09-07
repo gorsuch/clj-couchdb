c581a6780d0d019fd32c345a3fae600ac8c51f99
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-09-07 11:57:51 -0500

    we now pass user auth info in via a hash-map

--- src/clj_couchdb/core.clj
+++ src/clj_couchdb/core.clj
@@ -2,14 +2,14 @@
 	(:require [clj-http.client :as client]
 		      [clj-json.core :as json]))
 
-(defn fetch [base db id & [user pass]]
-	(json/parse-string (:body (client/get (str base "/" db "/" id) {:basic-auth [user pass]}))))
+(defn fetch [base db id & [params]]
+	(json/parse-string (:body (client/get (str base "/" db "/" id) {:basic-auth [(:user params) (:pass params)]}))))
 
-(defn insert [base db doc & [user pass]]
+(defn insert [base db doc & [params]]
 	(client/post (str base "/" db) {:body (json/generate-string doc)
-		                            :basic-auth [user pass]
+		                            :basic-auth [(:user params) (:pass params)]
 		                            :content-type :json
 		                            :accept :json}))
 
-(defn view [base db design-doc name & [params user pass]]
-	(json/parse-string (:body (client/get (str base "/" db "/_design/" design-doc "/_view/" name) {:basic-auth [user pass]}))))
+(defn view [base db design-doc name & [params]]
+	(json/parse-string (:body (client/get (str base "/" db "/_design/" design-doc "/_view/" name) {:basic-auth [(:user params) (:pass params)]}))))
