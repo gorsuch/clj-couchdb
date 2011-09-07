56d03d36b5a5f48013e921e36dca225aa246131f
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-09-07 11:58:39 -0500

    use 'auth-user' and 'auth-pass' instead of 'auth' and 'pass' to clear up any confusion

--- src/clj_couchdb/core.clj
+++ src/clj_couchdb/core.clj
@@ -3,13 +3,13 @@
 		      [clj-json.core :as json]))
 
 (defn fetch [base db id & [params]]
-	(json/parse-string (:body (client/get (str base "/" db "/" id) {:basic-auth [(:user params) (:pass params)]}))))
+	(json/parse-string (:body (client/get (str base "/" db "/" id) {:basic-auth [(:auth-user params) (:auth-pass params)]}))))
 
 (defn insert [base db doc & [params]]
 	(client/post (str base "/" db) {:body (json/generate-string doc)
-		                            :basic-auth [(:user params) (:pass params)]
+		                            :basic-auth [(:auth-user params) (:auth-pass params)]
 		                            :content-type :json
 		                            :accept :json}))
 
 (defn view [base db design-doc name & [params]]
-	(json/parse-string (:body (client/get (str base "/" db "/_design/" design-doc "/_view/" name) {:basic-auth [(:user params) (:pass params)]}))))
+	(json/parse-string (:body (client/get (str base "/" db "/_design/" design-doc "/_view/" name) {:basic-auth [(:auth-user params) (:auth-pass params)]}))))
