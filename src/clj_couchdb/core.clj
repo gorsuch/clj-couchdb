42fdeb2c35d5b9aaa75bba88eb0ecf3e76721f08
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-03-01 16:28:29 -0600

    whitespace

--- src/clj_couchdb/core.clj
+++ src/clj_couchdb/core.clj
@@ -3,12 +3,15 @@
 		  [cheshire.core :as json]))
 
 (defn fetch [db id & [params]]
-	(json/parse-string (:body (client/get (str db "/" id) params)) true))
+  (json/parse-string (:body (client/get (str db "/" id) params)) true))
 
 (defn insert [db doc & [params]]
-	(client/post db (merge {:body (json/generate-string doc)
-		                                   :content-type :json
-		                                   :accept :json} params)))
+  (client/post
+   db
+   (merge {:body (json/generate-string doc)
+           :content-type :json
+           :accept :json}
+          params)))
 
 (defn view [db design-doc name & [params]]
-	(json/parse-string (:body (client/get (db "/_design/" design-doc "/_view/" name) params))))
+  (json/parse-string (:body (client/get (db "/_design/" design-doc "/_view/" name) params))))
